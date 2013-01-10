233e9b07a23d9cc8b02dbd0c75150c40c5b03129
James Harris <stoffle@gmail.com>
2013-01-11 01:19:25 +1000

    Implemented Set (#8)

---
+++ lib/Icecave/Collections/Set.php
@@ -0,0 +1,391 @@
+<?php
+namespace Icecave\Collections;
+
+use Icecave\Collections\Support\Stringify;
+
+class Set implements MutableIterableInterface
+{
+    /**
+     * @param traversable|null $collection An iterable type containing the elements to include in this set, or null to create an empty set.
+     * @param callable|null $hashFunction The function to use for generating hashes of elements, or null to use the default.
+     */
+    public function __construct($collection = null, $hashFunction = null)
+    {
+        if (null === $hashFunction) {
+            $hashFunction = new AssociativeKeyGenerator;
+        }
+
+        $this->hashFunction = $hashFunction;
+        $this->elements = array();
+
+        if (null !== $collection) {
+            foreach ($collection as $element) {
+                $this->add($element);
+            }
+        }
+    }
+
+    ///////////////////////////////////////////
+    // Implementation of CollectionInterface //
+    ///////////////////////////////////////////
+
+    /**
+     * Fetch the number of elements in the collection.
+     *
+     * @see Set::isEmpty()
+     *
+     * @return integer The number of elements in the collection.
+     */
+    public function size()
+    {
+        return count($this->elements);
+    }
+
+    /**
+     * Check if the collection is empty.
+     *
+     * @return boolean True if the collection is empty; otherwise, false.
+     */
+    public function isEmpty()
+    {
+        return empty($this->elements);
+    }
+
+    /**
+     * Fetch a string representation of the collection.
+     *
+     * The string may not describe all elements of the collection, but should at least
+     * provide information on the type and state of the collection.
+     *
+     * @return string A string representation of the collection.
+     */
+    public function __toString()
+    {
+        if ($this->isEmpty()) {
+            return '<Set 0>';
+        }
+
+        $elements = array();
+        $index = 0;
+        foreach ($this->elements as $element) {
+            if ($index++ === 3) {
+                break;
+            }
+
+            $elements[] = Stringify::stringify($element);
+        }
+
+        if ($this->size() > 3) {
+            $format = '<Set %d [%s, ...]>';
+        } else {
+            $format = '<Set %d [%s]>';
+        }
+
+        return sprintf(
+            $format,
+            $this->size(),
+            implode(', ', $elements)
+        );
+    }
+
+    //////////////////////////////////////////////////
+    // Implementation of MutableCollectionInterface //
+    //////////////////////////////////////////////////
+
+    /**
+     * Remove all elements from the collection.
+     */
+    public function clear()
+    {
+        $this->elements = array();
+    }
+
+    /////////////////////////////////////////
+    // Implementation of IterableInterface //
+    /////////////////////////////////////////
+
+    /**
+     * Fetch a native array containing the elements in the collection.
+     *
+     * @return array An array containing the elements in the collection.
+     */
+    public function elements()
+    {
+        return array_values($this->elements);
+    }
+
+    /**
+     * Check if the collection contains an element with the given value.
+     *
+     * @param mixed $value The value to check.
+     *
+     * @return boolean True if the collection contains $value; otherwise, false.
+     */
+    public function contains($value)
+    {
+        $hash = $this->generateHash($value);
+        return array_key_exists($hash, $this->elements);
+    }
+
+    /**
+     * Fetch a new collection with a subset of the elements from this collection.
+     *
+     * The predicate must be a callable with the following signature:
+     *  function (mixed $element) { return $true_to_retain_element; }
+     *
+     * @param callable|null $predicate A predicate function used to determine which elements to include, or null to include all non-null elements.
+     *
+     * @return Set The filtered collection.
+     */
+    public function filtered($predicate = null)
+    {
+        if (null === $predicate) {
+            $predicate = function ($element) {
+                return null !== $element;
+            };
+        }
+
+        $result = new static(null, $this->hashFunction);
+
+        foreach ($this->elements as $element) {
+            if (call_user_func($predicate, $element)) {
+                $result->add($element);
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Produce a new collection by applying a transformation to each element.
+     *
+     * The new elements produced by the transform need not be of the same type.
+     * It is not guaranteed that the concrete type of the resulting collection will match this collection.
+     *
+     * The transform must be a callable with the following signature:
+     *  function (mixed $element) { return $new_element; }
+     *
+     * @param callable $transform The transform to apply to each element.
+     *
+     * @return IterableInterface A new collection produced by applying $transform to each element in this collection.
+     */
+    public function map($transform)
+    {
+        $result = new static(null, $this->hashFunction);
+
+        foreach ($this->elements as $element) {
+            $result->add(call_user_func($transform, $element));
+        }
+
+        return $result;
+    }
+
+    ////////////////////////////////////////////////
+    // Implementation of MutableIterableInterface //
+    ////////////////////////////////////////////////
+
+    /**
+     * Filter this collection in-place.
+     *
+     * The predicate must be a callable with the following signature:
+     *  function (mixed $element) { return $true_to_retain_element; }
+     *
+     * @param callable|null $predicate A predicate function used to determine which elements to retain, or null to retain all elements with non-null values.
+     */
+    public function filter($predicate = null)
+    {
+        if (null === $predicate) {
+            $predicate = function ($element) {
+                return null !== $element;
+            };
+        }
+
+        foreach ($this->elements as $hash => $element) {
+            if (!call_user_func($predicate, $element)) {
+                unset($this->elements[$hash]);
+            }
+        }
+    }
+
+    /**
+     * Replace each element in the collection with the result of a transformation on that element.
+     *
+     * The new elements produced by the transform must be the same type.
+     *
+     * The transform must be a callable with the following signature:
+     *  function (mixed $element) { return $element; }
+     *
+     * @param callable $transform The transform to apply to each element.
+     */
+    public function apply($transform)
+    {
+        $result = $this->map($transform);
+        $this->elements = $result->elements;
+    }
+
+    /**
+     * Return the first of the given elements that is contained in the set.
+     *
+     * @param mixed,... $element The elements to search for.
+     *
+     * @return mixed The first of the given elements that is contained in the set.
+     * @throws Exception\UnknownKeyException if none of the elements exist.
+     */
+    public function cascade($element)
+    {
+        return $this->cascadeIterable(func_get_args());
+    }
+
+    /**
+     * Return the first of the given elements that is contained in the set, or a default if none are found.
+     *
+     * @param mixed,... $element The elements to search for.
+     * @param mixed $default The default value to return if no such elements exist.
+     *
+     * @return mixed The first of the given elements that is contained in the set, or $default if none are found.
+     */
+    public function cascadeWithDefault($element, $default)
+    {
+        $elements = func_get_args();
+        $default = array_pop($elements);
+
+        return $this->cascadeIterableWithDefault($elements, $default);
+    }
+
+    /**
+     * Return the first of the given elements that is contained in the set.
+     *
+     * Behaves as per {@see Set::cascade()} except that the elements are provided as
+     * a traversable (eg, array) instead of via a variable argument list.
+     *
+     * @param traversable $elements The list of elements.
+     *
+     * @return mixed The first of the given elements that is contained in the set.
+     * @throws Exception\UnknownKeyException if none of the elements exist.
+     */
+    public function cascadeIterable($elements)
+    {
+        foreach ($elements as $element) {
+            $hash = $this->generateHash($element);
+            if (array_key_exists($hash, $this->elements)) {
+                return $element;
+            }
+        }
+
+        throw new Exception\UnknownKeyException($element);
+    }
+
+    /**
+     * Return the first of the given elements that is contained in the set, or a default if none are found.
+     *
+     * Behaves as per {@see Set::cascadeDefault()} except that the elements are provided as
+     * a traversable (eg, array) instead of via a variable argument list.
+     *
+     * @param traversable $elements The list of elements.
+     * @param mixed $default The default value to return if no such elements exist.
+     *
+     * @return mixed The first of the given elements that is contained in the set, or $default if none are found.
+     */
+    public function cascadeIterableWithDefault($elements, $default = null)
+    {
+        foreach ($elements as $element) {
+            $hash = $this->generateHash($element);
+            if (array_key_exists($hash, $this->elements)) {
+                return $element;
+            }
+        }
+
+        return $default;
+    }
+
+    public function add($element)
+    {
+        $hash = $this->generateHash($element);
+        if (array_key_exists($hash, $this->elements)) {
+            return false;
+        }
+
+        $this->elements[$hash] = $element;
+        return true;
+    }
+
+    public function remove($element)
+    {
+        $hash = $this->generateHash($element);
+        if (array_key_exists($hash, $this->elements)) {
+            unset($this->elements[$hash]);
+            return true;
+        }
+        return false;
+    }
+
+    public function union($elements)
+    {
+        $result = clone $this;
+        $result->unionInPlace($elements);
+        return $result;
+    }
+
+    public function unionInPlace($elements)
+    {
+        if ($elements instanceof self && $this->hashFunction == $elements->hashFunction) {
+            $this->elements += $elements->elements;
+        } else {
+            foreach ($elements as $element) {
+                $this->add($element);
+            }
+        }
+    }
+
+    public function intersect($elements)
+    {
+        $result = clone $this;
+        $result->intersectInPlace($elements);
+        return $result;
+    }
+
+    public function intersectInPlace($elements)
+    {
+        if ($elements instanceof self && $this->hashFunction == $elements->hashFunction) {
+            $this->elements = array_intersect_assoc($this->elements, $elements->elements);
+        } else {
+            $newElements = array();
+
+            foreach ($elements as $element) {
+                $hash = $this->generateHash($element);
+                if (array_key_exists($hash, $this->elements)) {
+                    $newElements[$hash] = $element;
+                }
+            }
+
+            $this->elements = $newElements;
+        }
+    }
+
+    public function complement($elements)
+    {
+        $result = clone $this;
+        $result->complementInPlace($elements);
+        return $result;
+    }
+
+    public function complementInPlace($elements)
+    {
+        if ($elements instanceof self && $this->hashFunction == $elements->hashFunction) {
+            $this->elements = array_diff_assoc($this->elements, $elements->elements);
+        } else {
+            foreach ($elements as $element) {
+                $hash = $this->generateHash($element);
+                unset($this->elements[$hash]);
+            }
+        }
+    }
+
+    protected function generateHash($key)
+    {
+        return call_user_func($this->hashFunction, $key);
+    }
+
+    private $hashFunction;
+    private $elements;
+}
---
+++ test/suite/Icecave/Collections/SetTest.php
@@ -0,0 +1,362 @@
+<?php
+namespace Icecave\Collections;
+
+use PHPUnit_Framework_TestCase;
+
+class SetTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->_collection = new Set;
+    }
+
+    public function testConstructor()
+    {
+        $this->assertSame(0, $this->_collection->size());
+    }
+
+    public function testConstructorWithArray()
+    {
+        $collection = new Set(array(1, 2, 3, 3, 4, 5));
+        $this->assertSame(array(1, 2, 3, 4, 5), $collection->elements());
+    }
+
+    ///////////////////////////////////////////
+    // Implementation of CollectionInterface //
+    ///////////////////////////////////////////
+
+    public function testSize()
+    {
+        $this->assertSame(0, $this->_collection->size());
+
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $this->assertSame(3, $this->_collection->size());
+
+        $this->_collection->clear();
+
+        $this->assertSame(0, $this->_collection->size());
+    }
+
+    public function testIsEmpty()
+    {
+        $this->assertTrue($this->_collection->isEmpty());
+
+        $this->_collection->add('a');
+
+        $this->assertFalse($this->_collection->isEmpty());
+
+        $this->_collection->clear();
+
+        $this->assertTrue($this->_collection->isEmpty());
+    }
+
+    public function testToString()
+    {
+        $this->assertSame('<Set 0>', $this->_collection->__toString());
+
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $this->assertSame("<Set 3 ['a', 'b', 'c']>", $this->_collection->__toString());
+
+        $this->_collection->add('d');
+
+        $this->assertSame("<Set 4 ['a', 'b', 'c', ...]>", $this->_collection->__toString());
+    }
+
+    //////////////////////////////////////////////////
+    // Implementation of MutableCollectionInterface //
+    //////////////////////////////////////////////////
+
+    public function testClear()
+    {
+        $this->_collection->add('a');
+
+        $this->_collection->clear();
+
+        $this->assertTrue($this->_collection->isEmpty());
+    }
+
+    /////////////////////////////////////////
+    // Implementation of IterableInterface //
+    /////////////////////////////////////////
+
+    public function testElements()
+    {
+        $this->assertSame(array(), $this->_collection->elements());
+
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $this->assertSame(array('a', 'b', 'c'), $this->_collection->elements());
+    }
+
+    public function testContains()
+    {
+        $this->assertFalse($this->_collection->contains('a'));
+
+        $this->_collection->add('a');
+
+        $this->assertTrue($this->_collection->contains('a'));
+    }
+
+    public function testFiltered()
+    {
+        $this->_collection->add('a');
+        $this->_collection->add(null);
+        $this->_collection->add('c');
+
+        $result = $this->_collection->filtered();
+
+        $this->assertInstanceOf(__NAMESPACE__ . '\Set', $result);
+        $this->assertSame(array('a', 'c'), $result->elements());
+    }
+
+    public function testFilteredWithPredicate()
+    {
+        $this->_collection->add(1);
+        $this->_collection->add(2);
+        $this->_collection->add(3);
+        $this->_collection->add(4);
+        $this->_collection->add(5);
+
+        $result = $this->_collection->filtered(
+            function ($value) {
+                return $value & 0x1;
+            }
+        );
+
+        $this->assertInstanceOf(__NAMESPACE__ . '\Set', $result);
+        $this->assertSame(array(1, 3, 5), $result->elements());
+    }
+
+    public function testMap()
+    {
+        $this->_collection->add(1);
+        $this->_collection->add(2);
+        $this->_collection->add(3);
+
+        $result = $this->_collection->map(
+            function ($value) {
+                return $value + 1;
+            }
+        );
+
+        $this->assertInstanceOf(__NAMESPACE__ . '\Set', $result);
+        $this->assertSame(array(2, 3, 4), $result->elements());
+    }
+
+    ////////////////////////////////////////////////
+    // Implementation of MutableIterableInterface //
+    ////////////////////////////////////////////////
+
+    public function testFilter()
+    {
+        $this->_collection->add('a');
+        $this->_collection->add(null);
+        $this->_collection->add('c');
+
+        $this->_collection->filter();
+
+        $this->assertSame(array('a', 'c'), $this->_collection->elements());
+    }
+
+    public function testFilterWithPredicate()
+    {
+        $this->_collection->add(1);
+        $this->_collection->add(2);
+        $this->_collection->add(3);
+        $this->_collection->add(4);
+        $this->_collection->add(5);
+
+        $this->_collection->filter(
+            function ($value) {
+                return $value & 0x1;
+            }
+        );
+
+        $this->assertSame(array(1, 3, 5), $this->_collection->elements());
+    }
+
+    public function testApply()
+    {
+        $this->_collection->add(1);
+        $this->_collection->add(2);
+        $this->_collection->add(3);
+
+        $this->_collection->apply(
+            function ($value) {
+                return $value + 1;
+            }
+        );
+
+        $this->assertSame(array(2, 3, 4), $this->_collection->elements());
+    }
+
+    public function testCascade()
+    {
+        $this->_collection->add('b');
+
+        $this->assertSame('b', $this->_collection->cascade('a', 'b', 'c'));
+    }
+
+    public function testCascadeFailure()
+    {
+        $this->setExpectedException(__NAMESPACE__ . '\Exception\UnknownKeyException', "Key 'c' does not exist.");
+
+        $this->_collection->cascade('a', 'b', 'c');
+    }
+
+    public function testCascadeWithDefault()
+    {
+        $this->assertSame('<default>', $this->_collection->cascadeWithDefault('a', 'b', 'c', '<default>'));
+
+        $this->_collection->add('b');
+
+        $this->assertSame('b', $this->_collection->cascadeWithDefault('a', 'b', 'c', '<default>'));
+    }
+
+    public function testCascadeIterable()
+    {
+        $this->_collection->add('b');
+
+        $this->assertSame('b', $this->_collection->cascadeIterable(array('a', 'b', 'c')));
+    }
+
+    public function testCascadeIterableWithDefault()
+    {
+        $this->assertSame('<default>', $this->_collection->cascadeIterableWithDefault(array('a', 'b', 'c'), '<default>'));
+
+        $this->_collection->add('b');
+
+        $this->assertSame('b', $this->_collection->cascadeIterableWithDefault(array('a', 'b', 'c'), '<default>'));
+    }
+
+    public function testAdd()
+    {
+        $this->assertFalse($this->_collection->contains('a'));
+
+        $this->assertTrue($this->_collection->add('a'));
+
+        $this->assertTrue($this->_collection->contains('a'));
+
+        $this->assertFalse($this->_collection->add('a'));
+
+        $this->assertTrue($this->_collection->contains('a'));
+    }
+
+    public function testRemove()
+    {
+        $this->assertFalse($this->_collection->remove('a'));
+
+        $this->_collection->add('a');
+
+        $this->assertTrue($this->_collection->remove('a'));
+
+        $this->assertFalse($this->_collection->contains('a'));
+    }
+
+    public function testUnion()
+    {
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $set = new Set;
+        $set->add('c');
+        $set->add('d');
+        $set->add('e');
+
+        $result = $this->_collection->union($set);
+
+        $this->assertSame(array('a', 'b', 'c', 'd', 'e'), $result->elements());
+    }
+
+    public function testUnionWithArray()
+    {
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $array = array(
+            'c',
+            'd',
+            'e'
+        );
+
+        $result = $this->_collection->union($array);
+
+        $this->assertSame(array('a', 'b', 'c', 'd', 'e'), $result->elements());
+    }
+
+    public function testIntersect()
+    {
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $set = new Set;
+        $set->add('c');
+        $set->add('d');
+        $set->add('e');
+
+        $result = $this->_collection->intersect($set);
+
+        $this->assertSame(array('c'), $result->elements());
+    }
+
+    public function testIntersectWithArray()
+    {
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $array = array(
+            'c',
+            'd',
+            'e'
+        );
+
+        $result = $this->_collection->intersect($array);
+
+        $this->assertSame(array('c'), $result->elements());
+    }
+
+    public function testComplement()
+    {
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $set = new Set;
+        $set->add('c');
+        $set->add('d');
+        $set->add('e');
+
+        $result = $this->_collection->complement($set);
+
+        $this->assertSame(array('a', 'b'), $result->elements());
+    }
+
+    public function testComplementWithArray()
+    {
+        $this->_collection->add('a');
+        $this->_collection->add('b');
+        $this->_collection->add('c');
+
+        $array = array(
+            'c',
+            'd',
+            'e'
+        );
+
+        $result = $this->_collection->complement($array);
+
+        $this->assertSame(array('a', 'b'), $result->elements());
+    }
+}
